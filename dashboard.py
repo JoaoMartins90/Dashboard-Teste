402c1291413817a7c8f2f6b57eed6a4b311488e5
joaoMartins90 <mjoao9651@gmail.com>
2024-05-10 10:14:20 -0300

    base

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,16 @@
-print('Hello World')
+import streamlit as st
+import requests
+import pandas as pd
+import plotly.express as px
+
+st.title(' DASHBOARD DE VENDA :shopping_trolley:')
+
+url = 'https://labdados.com/produtos'
+response = requests.get(url)
+dados = pd.DataFrame.from_dict(response.json())
+
+st.metric('Receitas', dados['Preço'].sum())
+st.metric('Quantidade de Vendas', dados.shape[0])
+
+
+st.dataframe(dados)
